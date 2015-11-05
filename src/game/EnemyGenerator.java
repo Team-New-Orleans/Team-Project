907785b8dcbd568a092a47ca2a72553e12119f1e
Simooo93 <simooovanov@gmail.com>
2015-11-05 16:49:53 +0200

    EnemyGenerator
    just the class added for now . Needs implementation I will work on it

---
+++ src/game/EnemyGenerator.java
@@ -0,0 +1,18 @@
+package game;
+
+import gameObjects.Dinosaur;
+import gameObjects.FlyingDragon;
+
+public class EnemyGenerator {
+
+
+    public EnemyGenerator() {
+        Handler.objects.add(new FlyingDragon(800, 30 ,false));
+        Handler.objects.add(new Dinosaur(800, 420 , false, player))
+    }
+
+    public void generatingEnemy(){
+        if()
+    }
+
+}
