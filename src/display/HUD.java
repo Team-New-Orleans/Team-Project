9b757375bb0b255e3e3be0e078d46bb0dcc5dbba
Simooo93 <simooovanov@gmail.com>
2015-11-07 10:31:32 +0200

    HUD error fixed and face added

--- src/display/HUD.java
+++ src/display/HUD.java
@@ -1,6 +1,7 @@
 package display;
 
 import game.Game;
+import gfx.ImageLoader;
 
 import java.awt.*;
 
@@ -14,13 +15,17 @@ public class HUD {
         if(Game.player.getHealth() < 200){
             greenValue = Game.player.getHealth();
         }
+        if(Game.player.getHealth() < 0){
+            greenValue = 0;
+        }
     }
     public void render(Graphics graphics){
         graphics.setColor(Color.gray);
-        graphics.fillRect(15, 15, 200, 32);
+        graphics.fillRect(35, 15, 200, 32);
         graphics.setColor(new Color(100, greenValue, 0));
-        graphics.fillRect(15, 15, Game.player.getHealth(), 32);
+        graphics.fillRect(35, 15, Game.player.getHealth(), 32);
         graphics.setColor(Color.white);
-        graphics.drawRect(15, 15, 200, 32);
+        graphics.drawRect(35, 15, 200, 32);
+        graphics.drawImage(ImageLoader.loadImage("/face.png"), 5, 15, null);
     }
 }
